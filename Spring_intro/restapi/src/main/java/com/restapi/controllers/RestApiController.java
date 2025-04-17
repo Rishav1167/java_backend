e7c46e3cac1f138a17323debb1f8db1d2135d44e
Rishav1167 <Rishav1167>
2025-04-17 11:08:22 +0530

    [Rishav] Spring Rest API UC 4 : added DTOs and check post method

--- Spring_intro/restapi/src/main/java/com/restapi/controllers/RestApiController.java
+++ Spring_intro/restapi/src/main/java/com/restapi/controllers/RestApiController.java
@@ -1,11 +1,11 @@
 package com.restapi.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import com.restapi.dtos.UserDto;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("hello")
 public class RestApiController {
     @GetMapping("/")
     public String index() {
@@ -21,4 +21,10 @@ public class RestApiController {
     public String param(@PathVariable String name) {
         return "Hello " + name;
     }
+
+    @PostMapping("/post")
+    public String post(@RequestBody UserDto userDTO) {
+        return "Hello " + userDTO.getFirstName() + " " + userDTO.getLastName();
+    }
+
 }
